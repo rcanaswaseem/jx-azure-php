07755c7c250e25285000c9d685b0ab0d20ccfe70
rcanaswaseem <anas.waseem@royalcyber.com>
2019-05-10 19:43:30 +0500

    Third Commit

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, World from Kubernetes! It works!<br>";
-echo "Hello Jenkins X! <br>";
+echo "Hello Jenkins X! It works too<br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
